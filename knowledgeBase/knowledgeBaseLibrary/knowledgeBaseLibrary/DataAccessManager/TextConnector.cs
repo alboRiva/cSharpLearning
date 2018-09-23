2a37a17361a57293fa4b7027d428778e6df633f8
albo <riva.alberto.96@gmail.com>
2018-09-23 18:37:25 +0200

    textConnector - beginning

---
+++ knowledgeBase/knowledgeBaseLibrary/knowledgeBaseLibrary/DataAccessManager/TextConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knowledgeBaseLibrary.DataAccessManager
+{
+    class TextConnector : IDataConnection
+    {
+        public Post CreatePost(Post submittedPost)
+        {
+            //store the info in the same spot - always configurated in App.config
+            //I need to read the file in order to find the ID
+            //Load Text file
+            //Convert to List<Post>
+            //Find max ID
+            //Add record with max ID + 1 to list<Post>
+            //Convert Post to list<strings>
+            //Save list<string> to text file
+            return submittedPost;
+        }
+    }
+}
---
+++ knowledgeBase/knowledgeBaseLibrary/knowledgeBaseLibrary/DataAccessManager/TextConnectorProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knowledgeBaseLibrary.DataAccessManager.TextConnector
+{
+    public static class TextConnectorProcessor
+    {
+        /// <summary>
+        /// takes in fileName and returns fullPath - extension method
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static string FullFilePath(this string fileName)  //Extension method
+        {
+            return $"{ ConfigurationManager.AppSettings["filePath"] }\\{ fileName }";
+        }
+
+        /// <summary>
+        /// Return a list<string> with all the lines in the file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<Post> convertToPost(this List<string> lines)
+        {
+
+        }
+    }
+}
