7e3ff880ddef2c7e43ca185ee2655c783442bc91
Alberto Riva <riva.alberto.96@gmail.com>
2018-10-29 14:36:50 +0100

    connessione al Db funzionante

---
+++ knowledgeBaseApp/knowledgeBaseLibrary/DataAccess/SqlConnector.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using knowledgeBaseLibrary.Models;
+
+namespace knowledgeBaseLibrary.DataAccess
+{
+    public class SqlConnector : IDataConnection
+    {
+        private List<Post> _repository;
+        private readonly string _connectionString;
+
+        public SqlConnector(string connectionString)
+        {
+            _connectionString = connectionString;
+            LoadRepository();
+        }
+        public void AddOrUpdatePost(Post sumbittedPost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPost(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetPostList(IEnumerable<string> tags, int pageNumber = 0, int itemsPerPage = int.MaxValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadRepository()
+        {
+            var queryString = "SELECT * FROM Posts";
+            using (SqlConnection connection =
+                new SqlConnection(_connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                // Open the connection in a try/catch block. 
+                // Create and execute the DataReader, writing the result
+                // set to the console window.
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        Console.WriteLine("\t{0}\t{1}\t{2}",
+                            reader[0], reader[1], reader[2]);
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
